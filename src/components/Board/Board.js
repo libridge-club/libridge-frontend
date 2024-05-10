d74234314bb427ecd5a59f55b7cd6e7d8613b0bd
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2024-05-10 14:40:22 -0300

    [feat] draw random board and show cards and double dummy table

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
+import DoubleDummyTable from "../DoubleDummyTable/DoubleDummyTable";
 import Hand from "../Hand/Hand";
 export default function Board() {
-    function getHandFromServerPromise() {
-        console.log("Calling fetch");
-        return fetch('http://164.90.254.243:8080/randomHand')
+
+    function getBoardEntityFromServerPromise() {
+        // console.log("Calling fetch");
+        return fetch('http://localhost:8080/randomBoard')
             .then(response => {
                 // console.log("Success:");
                 // console.log(response);
@@ -11,11 +13,11 @@ export default function Board() {
             })
             .then(data => {
                 // console.log('Fetched data:', data);
-                return data.cards;
+                return data;
             })
             .catch(error => {
-                console.error('ERROR!');
-                console.error(error);
+                // console.error('ERROR!');
+                return console.error(error);
             });
     }
 
@@ -30,20 +32,16 @@ export default function Board() {
 
     useEffect(() => {
         let ignore = false;
-        function addKeyToCards(cards) {
-            return cards.map(card => {
-                return {
-                    ...card,
-                    "key": getCardUniqueKey(card)
-                }
-            })
-        }
         async function startFetching() {
-            getHandFromServerPromise().then(cards => {
+            getBoardEntityFromServerPromise().then(outsideBoard => {
+                console.log("BoardEntity from server");
+                console.log(outsideBoard);
                 if (!ignore) {
-                    const cardsArrayWithKey = addKeyToCards(cards)
                     console.log("Setting new cards!");
-                    setCards(cardsArrayWithKey);
+                    setAllHands(outsideBoard["board"]["hands"])
+                    console.log("Setting double dummy values:")
+                    console.log(outsideBoard["doubleDummyTable"])
+                    setDoubleDummyTableValues(outsideBoard["doubleDummyTable"])
                 }
             });
         }
@@ -63,24 +61,54 @@ export default function Board() {
         removeCard(key);
     }
 
+    function resetAllHands(){
+        setCardsNorth([]);
+        setCardsEast([]);
+        setCardsSouth([]);
+        setCardsWest([]);
+        setDoubleDummyTableValues([]);
+    }
+
+    function setAllHands(hands){
+        setCardsNorth(addKeyToCards(hands["NORTH"]["cards"]));
+        setCardsEast(addKeyToCards(hands["EAST"]["cards"]));
+        setCardsSouth(addKeyToCards(hands["SOUTH"]["cards"]));
+        setCardsWest(addKeyToCards(hands["WEST"]["cards"]));
+    }
+
     function handleDrawHand() {
-        setCards([]);
-        getHandFromServerPromise().then((cards) => {
+        resetAllHands();
+        getBoardEntityFromServerPromise().then((outsideBoard) => {
             console.log("Setting new cards!");
-            setCards(addKeyToCards(cards));
+            console.log(outsideBoard);
+            setAllHands(outsideBoard["board"]["hands"]);
+            console.log("Setting double dummy values:")
+            console.log(outsideBoard["doubleDummyTable"])
+            setDoubleDummyTableValues(outsideBoard["doubleDummyTable"])
         });
     }
 
     function removeCard(key) {
-        setCards(cards.filter((value) => value.key !== key));
+        setCardsNorth(cardsNorth.filter((value) => value.key !== key));
+        setCardsEast(cardsEast.filter((value) => value.key !== key));
+        setCardsSouth(cardsSouth.filter((value) => value.key !== key));
+        setCardsWest(cardsWest.filter((value) => value.key !== key));
     }
 
-    const [cards, setCards] = useState([]);
+    const [cardsNorth, setCardsNorth] = useState([]);
+    const [cardsEast, setCardsEast] = useState([]);
+    const [cardsSouth, setCardsSouth] = useState([]);
+    const [cardsWest, setCardsWest] = useState([]);
+    const [doubleDummyTableValues, setDoubleDummyTableValues] = useState({});
 
     return (
         <div className="Board">
             <button onClick={handleDrawHand}>Draw hand</button>
-            <Hand cards={cards} onClick={handleClick} />
+            <Hand cards={cardsNorth} onClick={handleClick} />
+            <Hand cards={cardsEast} onClick={handleClick} />
+            <Hand cards={cardsSouth} onClick={handleClick} />
+            <Hand cards={cardsWest} onClick={handleClick} />
+            <DoubleDummyTable values={doubleDummyTableValues} />
         </div>
     );
 
---
+++ src/components/DoubleDummyTable/DoubleDummyTable.js
@@ -0,0 +1,38 @@
+export default function DoubleDummyTable({ values }) {
+    const directions = ["NORTH","SOUTH","EAST","WEST"]
+    const strains = ["CLUBS","DIAMONDS","HEARTS","SPADES","NOTRUMPS"]
+    if(values==undefined || values==null || values["NORTH"]==null){
+        return (
+            <div className="DoubleDummyTable">
+                <p>Empty</p>
+            </div>
+        )
+    } else {
+        const header = (
+            <thead>
+                <tr>
+                    <th key="emptySpace"> </th>
+                    {strains.map(strain =>( <th key={"th_"+strain}>{strain.charAt(0)}</th> ))}
+                </tr>
+            </thead>
+        )
+        
+        const allRows = directions.map( direction => (
+            <tbody key={"tbody_"+direction}>
+                <tr>
+                    <td key={"td_"+direction}>{direction.charAt(0)}</td>
+                    {strains.map(strain => (<td key={direction + strain}>{values[direction][strain]}</td>))}
+                </tr>
+            </tbody>
+        ))
+
+        return (
+            <div className="DoubleDummyTable">
+                <table>
+                    {header}
+                    {allRows}
+                </table>
+            </div>
+        );
+    }
+}

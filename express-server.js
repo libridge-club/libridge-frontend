f8d9af3429057ca4f2222c8c94bdcb54a1e3f404
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2024-06-05 03:08:15 -0300

    [feat] add https server with express

---
+++ express-server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
+const keyLocation = process.env.KEY_LOCATION
+const certLocation = process.env.CERT_LOCATION
+const options = {
+  key: fs.readFileSync(keyLocation),
+  cert: fs.readFileSync(certLocation)
+};
+
+// Create a service (the app object is just a callback).
+const app = express();
+const httpsPort = 443
+
+const basePath = "/build"
+
+
+app.use(express.static(path.join(__dirname, basePath)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, basePath + '/index.html'));
+})
+
+console.log("creating server");
+https.createServer(options, app).listen(httpsPort);
+console.log("last line");
